9b40726dd90fdc451bf0399d18e3c6c263709dfe
Alexander Perkins <aperkins@freewheel.tv>
2019-11-07 22:04:07 -0500

    Sandbox

--- src/Main.elm
+++ src/Main.elm
@@ -3,8 +3,27 @@ module Main exposing (main)
 import Browser
 import Html.Styled as H
 
+
+main : Program () Model msg
 main =
-  H.h1
-    []
-    [ H.text "Hello, Friday Tech Lounge!" ]
-  |> H.toUnstyled
+    Browser.sandbox
+        { init = "Hello, Friday Tech Lounge!"
+        , update = update
+        , view = view >> H.toUnstyled
+        }
+
+
+type alias Model =
+    String
+
+
+update : msg -> Model -> Model
+update _ model =
+    model
+
+
+view : Model -> H.Html msg
+view model =
+    H.h1
+        []
+        [ H.text model ]
