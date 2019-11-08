155940ef39d446d4288ddc0c5b7dfba31e20086c
Alexander Perkins <aperkins@freewheel.tv>
2019-11-07 23:43:28 -0500

    two counters

--- src/Main.elm
+++ src/Main.elm
@@ -20,37 +20,62 @@ hitPointsEnd =
 main : Program () Model Msg
 main =
     Browser.sandbox
-        { init = hitPointsInit
+        { init = init
         , update = update
         , view = view >> H.toUnstyled
         }
 
 
 type alias Model =
-    Int
+    { firstHitPoints : Int
+    , otherHitPoints : Int
+    }
+
+
+init : Model
+init =
+    { firstHitPoints = hitPointsInit
+    , otherHitPoints = hitPointsInit
+    }
 
 
 type Msg
     = UserClickedDecrementButton
     | UserClickedIncrementButton
+    | UserClickedOtherDecrementButton
+    | UserClickedOtherIncrementButton
 
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
         UserClickedDecrementButton ->
-            if model <= hitPointsEnd then
-                hitPointsEnd
+            if model.firstHitPoints <= hitPointsEnd then
+                { model | firstHitPoints = hitPointsEnd }
 
             else
-                model - 1
+                { model | firstHitPoints = model.firstHitPoints - 1 }
 
         UserClickedIncrementButton ->
-            if model >= hitPointsInit then
-                hitPointsInit
+            if model.firstHitPoints >= hitPointsInit then
+                { model | firstHitPoints = hitPointsInit }
 
             else
-                model + 1
+                { model | firstHitPoints = model.firstHitPoints + 1 }
+
+        UserClickedOtherDecrementButton ->
+            if model.otherHitPoints <= hitPointsEnd then
+                { model | otherHitPoints = hitPointsEnd }
+
+            else
+                { model | otherHitPoints = model.otherHitPoints - 1 }
+
+        UserClickedOtherIncrementButton ->
+            if model.otherHitPoints >= hitPointsInit then
+                { model | otherHitPoints = hitPointsInit }
+
+            else
+                { model | otherHitPoints = model.otherHitPoints + 1 }
 
 
 view : Model -> H.Html Msg
@@ -63,6 +88,7 @@ view model =
         -- Decrement Button
         , H.button
             [ HE.onClick UserClickedDecrementButton
+            , HA.disabled <| model.firstHitPoints == hitPointsEnd
             , HA.css
                 [ Css.fontSize <| Css.em 4
                 , Css.display Css.inlineBlock
@@ -79,11 +105,49 @@ view model =
                 , Css.marginRight <| Css.em 0.33
                 ]
             ]
-            [ H.text <| String.fromInt model ]
+            [ H.text <| String.fromInt model.firstHitPoints ]
 
         -- Increment Button
         , H.button
             [ HE.onClick UserClickedIncrementButton
+            , HA.disabled <| model.firstHitPoints == hitPointsInit
+            , HA.css
+                [ Css.fontSize <| Css.em 4
+                , Css.display Css.inlineBlock
+                , Css.marginRight <| Css.em 0.33
+                ]
+            ]
+            [ H.text "+1" ]
+
+        -- Line!
+        , H.hr [] []
+
+        -- Decrement Button
+        , H.button
+            [ HE.onClick UserClickedOtherDecrementButton
+            , HA.disabled <| model.otherHitPoints == hitPointsEnd
+            , HA.css
+                [ Css.fontSize <| Css.em 4
+                , Css.display Css.inlineBlock
+                , Css.marginRight <| Css.em 0.33
+                ]
+            ]
+            [ H.text "-1" ]
+
+        -- Display
+        , H.div
+            [ HA.css
+                [ Css.fontSize <| Css.em 4
+                , Css.display Css.inlineBlock
+                , Css.marginRight <| Css.em 0.33
+                ]
+            ]
+            [ H.text <| String.fromInt model.otherHitPoints ]
+
+        -- Increment Button
+        , H.button
+            [ HE.onClick UserClickedOtherIncrementButton
+            , HA.disabled <| model.otherHitPoints == hitPointsInit
             , HA.css
                 [ Css.fontSize <| Css.em 4
                 , Css.display Css.inlineBlock
